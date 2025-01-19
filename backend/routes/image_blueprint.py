f3c67e9ebb434d72821cf2c9e98681aa48b9b63b
kyoshinoda <kyoshisew@gmail.com>
2025-01-19 17:09:57 -0500

    Finished story: (not working with postman however)

--- backend/routes/image_blueprint.py
+++ backend/routes/image_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from PIL import Image
 from http import HTTPStatus
+from business.image_utils import valid_file
 import io
 
 image_blueprint = Blueprint('image', __name__)
@@ -9,18 +10,21 @@ image_blueprint = Blueprint('image', __name__)
 def upload_image():
     if 'image' not in request.files:
         return jsonify({"error": "No photo provided!"}), HTTPStatus.BAD_REQUEST
-    
     file = request.files["image"]
+       
+    if file.filename == "" or not valid_file(file.filename):
+        return {"error": "Invalid file type"}, HTTPStatus.BAD_REQUEST
 
     try:
         image = Image.open(io.BytesIO(file.read()))
         image.verify()
-        
-        return jsonify({"message": "Image received!", "size": image.size}), HTTPStatus.OK
+        result = {
+            "message": "Image received!",
+            "format": image.format,
+            "size": image.size
+        }
+        print("RESULT: ", result)
+        return jsonify(result), HTTPStatus.OK
     
     except Exception as e:
         return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
-
-@image_blueprint.route('/', methods=["GET"])
-def get_image():
-    return "HI", HTTPStatus.OK
